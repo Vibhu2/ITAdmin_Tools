b3ffbb5034c09c907121aabaad1e127ba44cef3a
vibhu Bhatnagar <vibhu@pwsh.in>
2026-06-03 16:25:55 +0530

    Fixed and issue while script was wrongly formatted

--- VB.ServerInventory/Public/07_Printing_Shares/Get-VBSharePermission.ps1
+++ VB.ServerInventory/Public/07_Printing_Shares/Get-VBSharePermission.ps1
@@ -1,8 +1,7 @@
-Get-VBSharePermission
 # ============================================================
 # FUNCTION : Get-VBSmbSharePermission
 # MODULE   : VBTools
-# VERSION  : 1.0.0
+# VERSION  : 1.0.1
 # CHANGED  : 29-05-2026 -- Initial release
 # AUTHOR   : Vibhu
 # PURPOSE  : Returns SMB share-level and NTFS top-level ACEs as objects
